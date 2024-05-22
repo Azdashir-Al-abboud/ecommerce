7628d6a48022c2825d71e734669c79ff9ad4ef9d
Azdashir-Al-abboud <azdashiraboud@gmail.com>
2024-05-22 23:19:14 +0300

    update index.js

--- connection.js
+++ connection.js
@@ -1,22 +1,39 @@
 const mysql = require("mysql2");
 require("dotenv").config();
-
-
-var connection = mysql.createConnection({
+const pool = mysql.createPool({
   port: process.env.DB_PORT,
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-
-connection.connect((err) => {
-  if (!err) {
-    console.log("Connected");
-  } else {
-    console.log(err);
-  }
+pool.getConnection((err, conn) => {
+  if (err) console.log(err);
+  console.log("Connected successfully");
 });
 
-module.exports = connection;
+module.exports = pool.promise();
+
+// require("dotenv").config();
+
+// var connection = mysql.createConnection({
+//   port: process.env.DB_PORT,
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// connection.connect((err) => {
+//   if (!err) {
+//     console.log("Connected");
+//   } else {
+//     console.log(err);
+//   }
+// });
+
+// module.exports = connection;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connection = require("./connection");
+require("dotenv").config();
 const userAuth = require("./auth/user");
 
 const app = express();
@@ -12,15 +13,20 @@ const upload = multer(); // =====
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// ==========
+
 // for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true })); 
 
 // for parsing multipart/form-data
 app.use(upload.array()); 
 app.use(express.static('public'));
-// ===========
+
+
 app.use("/user", userAuth);
 
+const PORT = process.env.PORT || 5000
 
-module.exports = app;
+app.listen(PORT, () => {
+    console.log("Server is running....")
+})
+// module.exports = app;
