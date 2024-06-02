83fc6c6f8abf61bc96b1305d3a9b2a77c3767624
Azdashir-Al-abboud <azdashiraboud@gmail.com>
2024-06-02 21:44:41 +0300

    o

--- auth/user.js
+++ auth/user.js
@@ -29,15 +29,15 @@ router.post("/signup",async (req, res) => {
                 .status(200)
                 .json({ message: "Successfully Registered" });
             } else {
-              // return res.status(500).json(err);
+              return res.status(500).json(err);
             }
           }
         );
-        await sendVerificationEmail({
-          to: user.email,
-          subject: "Verify Code Ecommerce",
-          html: `<b>Hey there! </b><br> Your Verify Code: ${verifyCode}<br/>`,
-        })
+        // await sendVerificationEmail({
+        //   to: user.email,
+        //   subject: "Verify Code Ecommerce",
+        //   html: `<b>Hey there! </b><br> Your Verify Code: ${verifyCode}<br/>`,
+        // });
       } else {
         return res
           .status(400)
