69c606dca7b38e73472e223d6f9c9c5a661a6f40
Azdashir-Al-abboud <azdashiraboud@gmail.com>
2024-06-03 13:45:05 +0300

    send mail

--- functions/sendemail.js
+++ functions/sendemail.js
@@ -29,53 +29,53 @@ const sendVerificationEmail = async ({ to, subject, html }) => {
   });
 };
 
-route.post("/text-mail", (req, res) => {
-  const { to, subject, text } = req.body;
-  const mailData = {
-    from: "azdashirsupport@gmail.com",
-    to: to,
-    subject: subject,
-    // text: text,
-    html: "<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>",
-  };
+// route.post("/text-mail", (req, res) => {
+//   const { to, subject, text } = req.body;
+//   const mailData = {
+//     from: "azdashirsupport@gmail.com",
+//     to: to,
+//     subject: subject,
+//     // text: text,
+//     html: "<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>",
+//   };
 
-  transporter.sendMail(mailData, (error, info) => {
-    if (error) {
-      console.log("=========");
-      return console.log(error);
-    }
-    res.status(200).send({ message: "Mail send", message_id: info.messageId });
-  });
-});
+//   transporter.sendMail(mailData, (error, info) => {
+//     if (error) {
+//       console.log("=========");
+//       return console.log(error);
+//     }
+//     res.status(200).send({ message: "Mail send", message_id: info.messageId });
+//   });
+// });
 
-route.post("/attachments-mail", (req, res) => {
-  const { to, subject, text } = req.body;
-  const mailData = {
-    from: "youremail@gmail.com",
-    to: to,
-    subject: subject,
-    text: text,
-    html: "<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>",
-    attachments: [
-      {
-        // file on disk as an attachment
-        filename: "nodemailer.png",
-        path: "nodemailer.png",
-      },
-      {
-        // file on disk as an attachment
-        filename: "text_file.txt",
-        path: "text_file.txt",
-      },
-    ],
-  };
+// route.post("/attachments-mail", (req, res) => {
+//   const { to, subject, text } = req.body;
+//   const mailData = {
+//     from: "youremail@gmail.com",
+//     to: to,
+//     subject: subject,
+//     text: text,
+//     html: "<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>",
+//     attachments: [
+//       {
+//         // file on disk as an attachment
+//         filename: "nodemailer.png",
+//         path: "nodemailer.png",
+//       },
+//       {
+//         // file on disk as an attachment
+//         filename: "text_file.txt",
+//         path: "text_file.txt",
+//       },
+//     ],
+//   };
 
-  transporter.sendMail(mailData, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    res.status(200).send({ message: "Mail send", message_id: info.messageId });
-  });
-});
+//   transporter.sendMail(mailData, (error, info) => {
+//     if (error) {
+//       return console.log(error);
+//     }
+//     res.status(200).send({ message: "Mail send", message_id: info.messageId });
+//   });
+// });
 
 module.exports = sendVerificationEmail;
