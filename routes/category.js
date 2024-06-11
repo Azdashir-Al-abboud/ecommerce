8b49ff84d8cfb3245763e398b00ea4fb4ce2f300
Azdashir-Al-abboud <azdashiraboud@gmail.com>
2024-06-12 00:56:37 +0300

    sort code

--- routes/category.js
+++ routes/category.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 router.get("/getall", (req, res) => {
     let user = req.body;
-    var query = "select * from users";
+    var query = "select * from categories";
     pool.query(query, (err, results) => {
       if (!err) {
           return res.status(200).json({ status: "success", data: results });
