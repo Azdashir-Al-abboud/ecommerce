a5c4e5a9b384121f1d651fcc5b06560d0f5cc82b
Azdashir-Al-abboud <azdashiraboud@gmail.com>
2024-06-12 13:55:17 +0300

    sort code

--- routes/home.js
+++ routes/home.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const pool = require("../connection");
 const router = express.Router();
-
 require("dotenv").config();
 
 router.get("/getall", (req, res) => {
-    let user = req.body;
-    var allData =[]
-    var query = "select * from categories";
-    pool.query(query, (err, results) => {
-      if (!err) {
-        allData['categories']=results
-        console.log(allData);
-          return res.status(200).json({ status: "success", data: allData });
-      } else {
-        return res.status(500).json({ status: "failure" }, err);
-      }
-    });
+  let user = req.body;
+  let allData = [];
+  var query = "select * from categories";
+  pool.query(query, (err, results) => {
+    if (!err) {
+      // allData["status"] = "success";
+      // allData["categories"] = results;
+      // console.log(allData);
+      return res.status(200).send({status:"success","categories":results});
+    } else {
+      return res.status(500).json({ status: "failure" }, err);
+    }
   });
+});
 
-  module.exports=router
+module.exports = router;
